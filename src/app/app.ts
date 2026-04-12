f04980b84f0c3125ef54f5251e6f859f8f3ab0ed
https://github.com/dedisyaputra-id <dedisyaputra611@gmail.com>
2026-04-13 05:40:52 +0700

    initial create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Schollproject');
+  //userName : string = "Dedi";
+  count : number = 0;
+  nama: string = "";
+  isLogin: boolean = false;
+
+
+  tambah () : number {
+    return this.count++;  
+  }
 }
